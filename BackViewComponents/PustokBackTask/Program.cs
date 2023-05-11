530a74db974e30e9e6c192eeb5b6e7bc1fb12419
umidnm <umidnm@code.edu.az>
2023-05-11 14:00:32 +0400

    Basket finished and ViewComponent Added

---
+++ BackViewComponents/PustokBackTask/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using PustokBackTask.DAL;
+using PustokBackTask.Services;
+
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlServer("Server=ELFRUSTAICHI\\SQLEXPRESS;Database=PustokTaskDB;Trusted_Connection=true");
+});
+
+builder.Services.AddScoped<LayoutService>();
+
+builder.Services.AddHttpContextAccessor();
+
+
+var app = builder.Build();
+
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ BackViewComponents/PustokBackTask/ViewComponents/NavGenresViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PustokBackTask.DAL;
+
+namespace PustokBackTask.ViewComponents
+{
+    public class NavGenresViewComponent
+    {
+        
+            private readonly DataContext _context;
+
+            public NavGenresViewComponent(DataContext context)
+            {
+                _context = context;
+            }
+            public IViewComponentResult Invoke()
+            {
+                var data = _context.Genres.ToList();
+
+                return View(data);
+            }
+        
+    }
+}
